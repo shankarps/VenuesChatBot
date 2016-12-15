e033d9fa80f92e2499b792ee96ff1c89417f5355
Shankar Pachari <shankar.pachari@gmail.com>
2016-12-15 12:16:24 -0800

    first version

---
+++ src/main/java/com/shankar/venues/VenuesChatBotApplication.java
@@ -0,0 +1,12 @@
+package com.shankar.venues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenuesChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenuesChatBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shankar/venues/VenuesChatBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shankar.venues;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VenuesChatBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
